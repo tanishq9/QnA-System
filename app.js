63ffcbd80c5f702f6ce09435baaf10f7bc11fab1
Tanishq <tanishqsaluja18@gmail.com>
2019-12-10 16:13:52 +0530

    added file checks

--- app.js
+++ app.js
@@ -13,33 +13,10 @@ app.use(express.urlencoded({extended:true}));
 
 app.use('/',express.static(__dirname+'/public'));
 
-app.get("/getAnswers",(req, res) => {
-
-  console.log('Time to run Python Script');
-  let options = {
-    "args": ['test.txt','ques.txt'] 
-  }
-  
-  output={};
-
-  PythonShell.run("main_text.py", options, (err, result) => {
-      if (err) {
-          throw err;
-      }
-      console.log(result);
-      index=0;
-      for(i=0;i<result.length;i++){
-        output[index++]=result[i];
-      }
-      res.send(output);
-      console.log(output);
-  });
-
-});
-
+// Website APIs
 
 app.post(
-  "/uploadPDF",
+  "/uploadContentTXT",
   multer({ dest: "./uploads" }).single("file-name"),
   (req, res) => {
     fs.unlinkSync("test.txt");
@@ -50,14 +27,34 @@ app.post(
           res.send("Success");
         } catch (e) {
           console.error(e);
-        }
-        
+        } 
     });
   }
 );
 
 app.post(
-  "/uploadTXT", multer({ dest: "./uploads" }).single("file-name"),
+  "/uploadContentPDF",
+  multer({ dest: "./uploads" }).single("file-name"),
+  (req, res) => {
+    fs.unlinkSync("test.txt");
+    fs.readFile(req.file.path, (err, pdfBuffer) => {
+      // pdfBuffer contains the file content
+      new PdfReader().parseBuffer(pdfBuffer, function(err, item) {
+        if (err) res.send(err);
+        else if (!item){
+          res.send("Done");
+        }  
+        else if (item.text) {
+          fs.writeFileSync("test.txt", item.text, { flag: "a" });
+          console.log(item.text);
+        }
+      });
+    });
+  }
+);
+
+app.post(
+  "/uploadQuestionsTXT", multer({ dest: "./uploads" }).single("file-name"),
   (req, res) => {
     fs.unlinkSync("ques.txt");
     fs.readFile(req.file.path, "utf8", function(err, data) {
@@ -68,7 +65,6 @@ app.post(
         } catch (e) {
           console.error(e);
         }
-        
     });
   }
 );
@@ -115,6 +111,33 @@ app.post(
       }) 
       res.send("Done");
 });
+
+// Get Answers API
+
+app.get("/getAnswers",(req, res) => {
+
+  console.log('Time to run Python Script');
+  let options = {
+    "args": ['test.txt','ques.txt'] 
+  }
+  
+  output={};
+
+  PythonShell.run("main_text.py", options, (err, result) => {
+      if (err) {
+          throw err;
+      }
+      console.log(result);
+      index=0;
+      for(i=0;i<result.length;i++){
+        output[index++]=result[i];
+      }
+      res.send(output);
+      console.log(output);
+  });
+
+});
+
 
 // Locally run the server
 app.listen(3000, () => {
--- public/main.js
+++ public/main.js
@@ -4,68 +4,114 @@ $(function(){
     $("#loader3").hide();
     $("#ans").hide();
 
-
+    // Submit Content File to Server
     $("#pdfSubmit").click(function(){
-        $("#pdfSubmit").attr("disabled", true);
-        $("#pdfSubmit").css("cursor", "default");
         var formData = new FormData();
         formData.append('file-name', $('#filePDF')[0].files[0]);
-        $("#loader").show();
-        
-        $.ajax({
-            url : 'uploadPDF',
-            type : 'POST',
-            data : formData,
-            processData: false,  // tell jQuery not to process the data
-            contentType: false,  // tell jQuery not to set contentType
-            success : function(data) {
-                console.log(data);
-                myFunction("Content File Uploaded Successfully")
-                $("#pdfSubmit").attr("value", "File Uploaded");
-                $("#loader").hide();
-            }
-        });
+        var fileName = $('#filePDF')[0].files[0].name;
+        console.log(fileName);
+        var extension = fileName.substr(fileName.length-3,fileName.length);
+        if(extension=="pdf"){
+            console.log("PDF Content File");
+            $("#pdfSubmit").attr("disabled", true);
+            $("#pdfSubmit").css("cursor", "default");
+            $("#loader").show();
+            $.ajax({
+                url : 'uploadContentPDF',
+                type : 'POST',
+                data : formData,
+                processData: false,  // tell jQuery not to process the data
+                contentType: false,  // tell jQuery not to set contentType
+                success : function(data) {
+                    console.log(data);
+                    myFunction("Content File Uploaded Successfully")
+                    $("#pdfSubmit").attr("value", "File Uploaded");
+                    $("#loader").hide();
+                    $("#filePDF").prop("disabled",true);
+                }
+            });
+        }else if(extension=="txt"){
+            console.log("TXT Content File");
+            $("#pdfSubmit").attr("disabled", true);
+            $("#pdfSubmit").css("cursor", "default");
+            $("#loader").show();
+            $.ajax({
+                url : 'uploadContentTXT',
+                type : 'POST',
+                data : formData,
+                processData: false,  // tell jQuery not to process the data
+                contentType: false,  // tell jQuery not to set contentType
+                success : function(data) {
+                    console.log(data);
+                    myFunction("Content File Uploaded Successfully")
+                    $("#pdfSubmit").attr("value", "File Uploaded");
+                    $("#loader").hide();
+                    $("#filePDF").prop("disabled",true);
+                }
+            });
+        }else{
+            myFunction("File Format NOT Supported")
+        } 
     });
 
+
+    // Submit Questions File to Server
     $("#txtSubmit").click(function(){
-        $("#txtSubmit").attr("disabled", true);
-        $("#txtSubmit").css("cursor", "default"); 
         // $("#loader2").show(); -- avoid showing the loader for text files as they take very little time to get uploaded
         var formData = new FormData();
         formData.append('file-name', $('#fileTXT')[0].files[0]);
-        console.log($('#fileTXT')[0].files[0].name);
-        $.ajax({
-            url : 'uploadTXT',
-            type : 'POST',
-            data : formData,
-            processData: false,  // tell jQuery not to process the data
-            contentType: false,  // tell jQuery not to set contentType
-            success : function(data) {
-                console.log(data);
-                $("#loader2").hide();
-                myFunction("Question(s) File Uploaded Successfully");
-                $("#txtSubmit").attr("value", "File Uploaded");
-            }
-        });
+        var fileName = $('#fileTXT')[0].files[0].name;
+        console.log(fileName);
+        var extension = fileName.substr(fileName.length-3,fileName.length);
+        if(extension=="txt"){
+            console.log("TXT Questions File");
+            $("#txtSubmit").attr("disabled", true);
+            $("#txtSubmit").css("cursor", "default"); 
+            $.ajax({
+                url : 'uploadQuestionsTXT',
+                type : 'POST',
+                data : formData,
+                processData: false,  // tell jQuery not to process the data
+                contentType: false,  // tell jQuery not to set contentType
+                success : function(data) {
+                    console.log(data);
+                    $("#loader2").hide();
+                    myFunction("Questions File Uploaded Successfully");
+                    $("#txtSubmit").attr("value", "File Uploaded");
+                    $("#fileTXT").prop("disabled",true);
+                }
+            });
+        }else{
+            myFunction("File Format NOT Supported")
+        } 
     });
 
+    // Submit 'Answer' Request to Server
     $("#answerButton").click(function(){
-        $("#loader3").show();
-        $.ajax({
-            url : 'getAnswers',
-            type : 'POST',
-            success : function(data) {
-                $("#loader3").hide();
-                $("#ans").show();
-                console.log(data.length);
-                var index,output="";
-                for(index=1;index<=(data.length-1);index+=2){
-                    console.log(data[index]);
-                    output+=(data[index]+"<hr>");                    
+        if($("#txtSubmit").attr("value")=="File Uploaded"  &&  $("#pdfSubmit").attr("value") == "File Uploaded"){
+            $("#loader3").show();
+            $("#answerButton").attr("disabled", true);
+            $("#answerButton").css("cursor", "default");
+            $.ajax({
+                url : 'getAnswers',
+                type : 'GET',
+                success : function(data) {
+                    $("#loader3").hide();
+                    $("#ans").show();
+                    var index,length=Object.keys(data).length,output="";
+                    console.log(length);
+                    for(index=1;index<length;index+=2){
+                        console.log(data[index]);
+                        output+=(data[index]+"<hr>");                          
+                    }
+                    $("#ans").html(output);
+                    $("#answerButton").attr("disabled", false);
+                    $("#answerButton").css("cursor", "pointer");
                 }
-                $("#ans").html(output);
-            }
-        });
+            });
+        }else{
+            myFunction("Upload All Files");
+        }
     });
 
 });
