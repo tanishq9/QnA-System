0a741f5b438f0502151b900c4d61e55434e5f6ee
Tanishq <tanishqsaluja18@gmail.com>
2019-12-11 02:37:06 +0530

    added empty field checks

--- public/main.js
+++ public/main.js
@@ -6,83 +6,91 @@ $(function(){
 
     // Submit Content File to Server
     $("#pdfSubmit").click(function(){
-        var formData = new FormData();
-        formData.append('file-name', $('#filePDF')[0].files[0]);
-        var fileName = $('#filePDF')[0].files[0].name;
-        console.log(fileName);
-        var extension = fileName.substr(fileName.length-3,fileName.length);
-        if(extension=="pdf"){
-            console.log("PDF Content File");
-            $("#pdfSubmit").attr("disabled", true);
-            $("#pdfSubmit").css("cursor", "default");
-            $("#loader").show();
-            $.ajax({
-                url : 'uploadContentPDF',
-                type : 'POST',
-                data : formData,
-                processData: false,  // tell jQuery not to process the data
-                contentType: false,  // tell jQuery not to set contentType
-                success : function(data) {
-                    console.log(data);
-                    myFunction("Content File Uploaded Successfully")
-                    $("#pdfSubmit").attr("value", "File Uploaded");
-                    $("#loader").hide();
-                    $("#filePDF").prop("disabled",true);
-                }
-            });
-        }else if(extension=="txt"){
-            console.log("TXT Content File");
-            $("#pdfSubmit").attr("disabled", true);
-            $("#pdfSubmit").css("cursor", "default");
-            $("#loader").show();
-            $.ajax({
-                url : 'uploadContentTXT',
-                type : 'POST',
-                data : formData,
-                processData: false,  // tell jQuery not to process the data
-                contentType: false,  // tell jQuery not to set contentType
-                success : function(data) {
-                    console.log(data);
-                    myFunction("Content File Uploaded Successfully")
-                    $("#pdfSubmit").attr("value", "File Uploaded");
-                    $("#loader").hide();
-                    $("#filePDF").prop("disabled",true);
-                }
-            });
+        if($("#filePDF").next("label").html()=="Choose Content File"){
+            myFunction("No File Chosen")
         }else{
-            myFunction("File Format NOT Supported")
+            var formData = new FormData();
+            formData.append('file-name', $('#filePDF')[0].files[0]);
+            var fileName = $('#filePDF')[0].files[0].name;
+            console.log(fileName);
+            var extension = fileName.substr(fileName.length-3,fileName.length);
+            if(extension=="pdf"){
+                console.log("PDF Content File");
+                $("#pdfSubmit").attr("disabled", true);
+                $("#pdfSubmit").css("cursor", "default");
+                $("#loader").show();
+                $.ajax({
+                    url : 'uploadContentPDF',
+                    type : 'POST',
+                    data : formData,
+                    processData: false,  // tell jQuery not to process the data
+                    contentType: false,  // tell jQuery not to set contentType
+                    success : function(data) {
+                        console.log(data);
+                        myFunction("Content File Uploaded Successfully")
+                        $("#pdfSubmit").attr("value", "File Uploaded");
+                        $("#loader").hide();
+                        $("#filePDF").prop("disabled",true);
+                    }
+                });
+            }else if(extension=="txt"){
+                console.log("TXT Content File");
+                $("#pdfSubmit").attr("disabled", true);
+                $("#pdfSubmit").css("cursor", "default");
+                $("#loader").show();
+                $.ajax({
+                    url : 'uploadContentTXT',
+                    type : 'POST',
+                    data : formData,
+                    processData: false,  // tell jQuery not to process the data
+                    contentType: false,  // tell jQuery not to set contentType
+                    success : function(data) {
+                        console.log(data);
+                        myFunction("Content File Uploaded Successfully")
+                        $("#pdfSubmit").attr("value", "File Uploaded");
+                        $("#loader").hide();
+                        $("#filePDF").prop("disabled",true);
+                    }
+                });
+            }else{
+                myFunction("File Format NOT Supported")
+            }
         } 
     });
 
 
     // Submit Questions File to Server
     $("#txtSubmit").click(function(){
-        // $("#loader2").show(); -- avoid showing the loader for text files as they take very little time to get uploaded
-        var formData = new FormData();
-        formData.append('file-name', $('#fileTXT')[0].files[0]);
-        var fileName = $('#fileTXT')[0].files[0].name;
-        console.log(fileName);
-        var extension = fileName.substr(fileName.length-3,fileName.length);
-        if(extension=="txt"){
-            console.log("TXT Questions File");
-            $("#txtSubmit").attr("disabled", true);
-            $("#txtSubmit").css("cursor", "default"); 
-            $.ajax({
-                url : 'uploadQuestionsTXT',
-                type : 'POST',
-                data : formData,
-                processData: false,  // tell jQuery not to process the data
-                contentType: false,  // tell jQuery not to set contentType
-                success : function(data) {
-                    console.log(data);
-                    $("#loader2").hide();
-                    myFunction("Questions File Uploaded Successfully");
-                    $("#txtSubmit").attr("value", "File Uploaded");
-                    $("#fileTXT").prop("disabled",true);
-                }
-            });
+        if($("#filePDF").next("label").html()=="Choose Content File"){
+            myFunction("No File Chosen")
         }else{
-            myFunction("File Format NOT Supported")
+            // $("#loader2").show(); -- avoid showing the loader for text files as they take very little time to get uploaded
+            var formData = new FormData();
+            formData.append('file-name', $('#fileTXT')[0].files[0]);
+            var fileName = $('#fileTXT')[0].files[0].name;
+            console.log(fileName);
+            var extension = fileName.substr(fileName.length-3,fileName.length);
+            if(extension=="txt"){
+                console.log("TXT Questions File");
+                $("#txtSubmit").attr("disabled", true);
+                $("#txtSubmit").css("cursor", "default"); 
+                $.ajax({
+                    url : 'uploadQuestionsTXT',
+                    type : 'POST',
+                    data : formData,
+                    processData: false,  // tell jQuery not to process the data
+                    contentType: false,  // tell jQuery not to set contentType
+                    success : function(data) {
+                        console.log(data);
+                        $("#loader2").hide();
+                        myFunction("Questions File Uploaded Successfully");
+                        $("#txtSubmit").attr("value", "File Uploaded");
+                        $("#fileTXT").prop("disabled",true);
+                    }
+                });
+            }else{
+                myFunction("File Format NOT Supported")
+            }    
         } 
     });
 
